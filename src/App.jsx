bf50ea59e0660462f0a1827621df45b6c1e0e036
Noppadon Tongtrakoon <notenoppadon5019@gmail.com>
2025-07-04 23:02:30 +0700

    Add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>Hello, Noppadon portfolio</h1>
+      <h1 class="bg-red-500 text-3xl font-bold underline">
+        Hello world!, tailwind
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
