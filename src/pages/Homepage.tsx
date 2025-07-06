37def701defc1929a4bd4bb75597dcf2d8e07357
Noppadon Tongtrakoon <notenoppadon5019@gmail.com>
2025-07-06 14:57:41 +0700

    Add end and Google analytics

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,11 +3,22 @@ import SkillSection from "../components/SkillSection";
 import CompaynySection from "../components/CompanySection";
 import HeroSectionV2 from "../components/HeroSectionV2";
 import MarqueeBar from "../components/MarqueeBar";
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
 
 export default function Homepage() {
+  const GA_ID = import.meta.env.VITE_GA_ID;
+
+  useEffect(() => {
+        ReactGA.initialize(GA_ID);
+        ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+      }, []);
+
+
   return (
     <>
       <HeroSectionV2 />
+      GA_ID = {GA_ID}
       <MarqueeBar />
       {/* <HeroSection /> */}
       <SkillSection />
