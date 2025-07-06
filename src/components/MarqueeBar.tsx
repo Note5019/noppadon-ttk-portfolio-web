9cb959e35ba973991211dfe017980317d31306d0
Noppadon Tongtrakoon <notenoppadon5019@gmail.com>
2025-07-06 14:39:43 +0700

    Add MarqueeBar component

---
+++ src/components/MarqueeBar.tsx
@@ -0,0 +1,22 @@
+import Marquee from "react-fast-marquee";
+
+export default function MarqueeBar() {
+  return (
+    <div className="relatve mb-6 py-6">
+      <div className="md:rotate-0.5 -mt-5 h-[50px] w-full bg-green-200"></div>
+      <div className="-mt-12 flex h-[50px] w-full -rotate-2 items-center justify-center bg-green-400 text-2xl font-bold text-white">
+        <Marquee
+          className="text-gray-200 dark:text-gray-200"
+          speed={20}
+          autoFill={true}
+          gradient={false}
+          play={true}
+          pauseOnHover={true}
+        >
+          <span className="mx-4">Full Stack Developer</span>
+          <span className="mx-4">Let's learn more about me!</span>
+        </Marquee>
+      </div>
+    </div>
+  );
+}
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,17 +2,13 @@ import HeroSection from "../components/HeroSection";
 import SkillSection from "../components/SkillSection";
 import CompaynySection from "../components/CompanySection";
 import HeroSectionV2 from "../components/HeroSectionV2";
+import MarqueeBar from "../components/MarqueeBar";
 
 export default function Homepage() {
   return (
     <>
       <HeroSectionV2 />
-      <div className="relatve mb-6 py-6">
-        <div className="-mt-5 h-[50px] w-full rotate-0.5 bg-green-200"></div>
-        <div className="-mt-12 h-[50px] w-full -rotate-2 bg-green-300 flex justify-center items-center text-white text-2xl font-bold">
-          Let's learn more about me!
-        </div>
-      </div>
+      <MarqueeBar />
       {/* <HeroSection /> */}
       <SkillSection />
       <CompaynySection />
